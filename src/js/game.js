72840fda0ac9ff93f9a6bda03d02d8c423dd55b8
RubenNN <ruben@neatnait.com>
2014-12-15 10:23:19 +0100

    First version

--- src/js/game.js
+++ src/js/game.js
@@ -9,15 +9,64 @@
 
     create: function () {
       var x = this.game.width / 2
-        , y = this.game.height / 2;
+        , y = this.game.height / 2
+        , i;
 
+      // Use the logo as player
       this.player = this.add.sprite(x, y, 'player');
       this.player.anchor.setTo(0.5, 0.5);
-      this.input.onDown.add(this.onInputDown, this);
+      //this.input.onDown.add(this.onInputDown, this);
+
+      this.game.physics.startSystem(Phaser.Physics.P2JS);
+      this.game.physics.p2.setImpactEvents(true);
+
+      this.playerCollisionGroup = this.game.physics.p2.createCollisionGroup();
+      this.boxCollisionGroup = this.game.physics.p2.createCollisionGroup();
+
+      this.game.physics.p2.updateBoundsCollisionGroup();
+
+      // Pixel Rain
+      this.boxes = this.game.add.group();
+      this.boxes.enableBody = true;
+      this.boxes.physicsBodyType = Phaser.Physics.P2JS;
+
+      for (i = 0; i < 30; i++){
+
+          var color = 'blue';
+
+          if(i%2){
+            color = 'red';
+          }
+          else if(i%3){
+            color = 'green';
+          }
+
+
+          var box = this.boxes.create(x, y*20, color);
+          box.body.setRectangle(20, 20);
+          box.scale.set(0.2);
+
+          box.body.setCollisionGroup(this.boxCollisionGroup);
+          box.body.collides([this.boxCollisionGroup, this.playerCollisionGroup]);
+      }
+
+      this.player.anchor.setTo(0.5, 0.5);
+      this.player.scale.set(0.3);
+
+      this.game.physics.p2.enable(this.player, true);
+      this.player.body.kinematic = true;
+
+      this.player.body.setCollisionGroup(this.playerCollisionGroup);
+
+      //  The ship will collide with the boxes, and when it strikes one the hitbox callback will fire, causing it to alpha out a bit
+      //  When boxes collide with each other, nothing happens to them.
+      this.player.body.collides(this.boxCollisionGroup, this.hitEnemy, this);
+      this.game.physics.p2.gravity.y = 500;
+      this.index = 0;
     },
 
     update: function () {
-      var x, y, cx, cy, dx, dy, angle, scale;
+      /*var x, y, cx, cy, dx, dy, angle, scale;
 
       x = this.input.position.x;
       y = this.input.position.y;
@@ -32,9 +81,25 @@
       scale = Math.sqrt(dx * dx + dy * dy) / 100;
 
       this.player.scale.x = scale * 0.6;
-      this.player.scale.y = scale * 0.6;
-    },
+      this.player.scale.y = scale * 0.6;*/
 
+      this.index++;
+      if(this.index === 20) {
+        this.index = 0;
+        this.addEnemy();
+      }
+      
+    },
+    addEnemy: function () {
+      var color = 'blue';
+
+      var box = this.boxes.create(this.game.width / 2, this.game.height*10, color);
+          box.body.setRectangle(20, 20);
+          box.scale.set(0.2);
+
+          box.body.setCollisionGroup(this.boxCollisionGroup);
+          box.body.collides([this.boxCollisionGroup, this.playerCollisionGroup]);
+    },
     onInputDown: function () {
       this.game.state.start('menu');
     }
--- src/js/preloader.js
+++ src/js/preloader.js
@@ -14,7 +14,10 @@
 
       this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
       this.load.setPreloadSprite(this.asset);
-      this.load.image('player', 'assets/player.png');
+      this.load.image('player', 'assets/logo.png');
+      this.load.image('blue', 'assets/blue.png');
+      this.load.image('red', 'assets/red.png');
+      this.load.image('green', 'assets/green.png');
       this.load.bitmapFont('minecraftia', 'assets/minecraftia.png', 'assets/minecraftia.xml');
     },
 
